7271e0b3594436d28430ca13fd6ce2363b859717
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-02 20:03:32 -0400

    Inicial commit

--- src/Lambdas/Mutiplicar.java
+++ src/Lambdas/Mutiplicar.java
@@ -6,4 +6,5 @@ public class Mutiplicar implements Calculo {
     public double executar(double a, double b) {
         return a * b;
     }
+
 }
--- src/Lambdas/Soma.java
+++ src/Lambdas/Soma.java
@@ -6,4 +6,5 @@ public class Soma implements Calculo {
     public double executar(double a, double b) {
         return a + b;
     }
+
 }
