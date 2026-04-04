82138561c69e0adc41cdd9a2dac48fd95ac58a0e
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-03 21:49:07 -0400

    Lambda

--- src/Lambdas/Consumidor.java
+++ src/Lambdas/Consumidor.java
@@ -1,8 +1,32 @@
 package Lambdas;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class Consumidor {
 
-   Consumer<String> imprimir = valor -> System.out.println(valor);
+    public static void main(String[] args) {
+
+        Consumer<Produto> imprimeNome = p -> System.out.println(p.nome + "!!!");
+
+        Produto p1 = new Produto("Caneta ", 12.34, 0.09);
+        imprimeNome.accept(p1);
+
+        Produto p2 = new Produto("Notebook ", 2987.99, 0.25);
+        Produto p3 = new Produto("Caderno ", 19.90, 0.03);
+        Produto p4 = new Produto("Borracha ", 7.80, 0.18);
+        Produto p5 = new Produto("Lapis", 4.39, 0.19);
+
+        List<Produto> produtos = Arrays.asList(p1, p2, p3, p4, p5);
+
+        produtos.forEach(imprimeNome);
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        produtos.forEach(p -> System.out.println(p.preco));
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        produtos.forEach(System.out::println);
+
+    }
+
+
 }
