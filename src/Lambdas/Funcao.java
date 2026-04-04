f13bd3be12002d49bfa2dffd9063af2d28499dc8
Felipe Cleverson <felipecleverson1@gmail.com>
2026-04-03 22:05:19 -0400

    lambda

---
+++ src/Lambdas/Funcao.java
@@ -0,0 +1,20 @@
+package Lambdas;
+
+import java.util.function.Function;
+
+public class Funcao {
+    public static void main(String[] args) {
+
+        Function<Integer, String> parOuImpar = numero -> numero % 2 == 0 ? "par" : "Impar";
+
+        System.out.println(parOuImpar.apply(32));
+
+        Function<String, String> empolgado = valor -> valor + "!!!!";
+
+        Function<String, String> oResultadoE = valor -> "O resultado e " + valor;
+
+        String resultadoFinal = parOuImpar.andThen(oResultadoE).andThen(empolgado).apply(32);
+        System.out.println(resultadoFinal);
+
+    }
+}
